67c5cc9b1a6eb263de8ea1e7b4c0560a7becf18c
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-06-20 09:42:33 +0700

    Migration Done

---
+++ database/migrations/2025_06_19_123301_create_matakuliahs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matakuliahs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kd_matakuliah')->unique();
+            $table->string('nama');
+            $table->string('semester');
+            $table->year('tahun');
+            $table->foreignId('lecturer_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matakuliah');
+    }
+};
