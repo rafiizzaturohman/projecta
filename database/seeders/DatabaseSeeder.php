32665b0b6c4584899c59178414bb8d6160a9366a
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-06-21 19:33:04 +0700

    User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        
         $this->call(UserSeeder::class);
         $this->call(MatakuliahSeeder::class);
         $this->call(ProjectSeeder::class);
