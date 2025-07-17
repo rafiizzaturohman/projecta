3574cebc84ceffcc43790276f07b3bd3144d532b
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-07-17 16:34:39 +0700

    Dashboard Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,12 +12,7 @@
                         @include('dashboard.admin')
 
                     @elseif ($role === 'dosen')
-                        <h3 class="text-xl font-bold mb-2">Dosen Dashboard</h3>
-                        <p>Welcome, {{ $userName }}! You can monitor student projects.</p>
-                        <ul class="list-disc list-inside mt-3 text-text-secondary dark:text-dark-text-secondary">
-                            <li>View Assigned Courses</li>
-                            <li>Review Project Submissions</li>
-                        </ul>
+                        @include('dashboard.dosen')
 
                     @elseif ($role === 'mahasiswa')
                         @include('dashboard.mahasiswa')
---
+++ resources/views/dashboard/dosen.blade.php
@@ -0,0 +1,10 @@
+<section>
+    <h3 class="text-xl font-bold mb-2">Dosen Dashboard</h3>
+    <p>Welcome, {{ $userName }}! You can monitor student projects.</p>
+    <ul
+        class="list-disc list-inside mt-3 text-text-secondary dark:text-dark-text-secondary"
+    >
+        <li>View Assigned Courses</li>
+        <li>Review Project Submissions</li>
+    </ul>
+</section>
